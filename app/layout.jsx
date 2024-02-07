164d02929114142a5a9f017841b45506f73cb59c
Ibai Arotçarena <arotcarena.ib@gmail.com>
2024-02-07 23:46:16 +0100

    config

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,5 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { montserrat_alternates } from "./ui/fonts";
 
 export const metadata = {
   title: "Portfolio",
@@ -11,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body className={inter.className}>{children}</body>
+      <body className={montserrat_alternates.className}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.js
@@ -0,0 +1,6 @@
+import { Montserrat_Alternates } from 'next/font/google';
+
+export const montserrat = Montserrat_Alternates({
+    weight: ['200', '300', '400', '500'],
+    subsets: ['latin']
+});
