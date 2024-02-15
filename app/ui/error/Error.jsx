93067cc26a992cf1f6c02d14764e6501c0e34553
Ibai Arotçarena <arotcarena.ib@gmail.com>
2024-02-15 22:31:10 +0100

    redeploiement

--- app/ui/error/Error.jsx
+++ app/ui/error/Error.jsx
@@ -4,6 +4,8 @@ import { Button } from '../buttons/Button';
  
 export default function Error({error, reset}) {
 
+    console.log(error);
+
     return (
         <div className="error-wrapper">
             <h2 className="error-title">Oups... Une erreur inattendue a eu lieu.</h2>
@@ -18,7 +20,7 @@ export default function Error({error, reset}) {
             }
             <div className="error-page-link-wrapper">
                 <Button onClick={reset} additionalClass={reset ? 'secondary': ''}>
-                    Retour à l'accueil
+                    Retour à l&apos;accueil
                 </Button>
             </div>
         </div>
