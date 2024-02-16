34a1efad243e1147efd9edd7ea2d6a51ed0be505
Ibai Arotçarena <arotcarena.ib@gmail.com>
2024-02-16 12:36:31 +0100

    deploiement

--- app/lib/SiteConfig.js
+++ app/lib/SiteConfig.js
@@ -1,7 +1,8 @@
 export class SiteConfig {
-    static API_URL = 'https://ia-portfolio-f661150a7212.herokuapp.com';
-
-    // static API_URL = 'https://api.portfolio.orawebsolutions.com';
-
+    
+    static API_URL = 'https://api.portfolio.orawebsolutions.com';
+    
+    // static API_URL = 'https://ia-portfolio-f661150a7212.herokuapp.com';
+    
     // static API_URL = 'http://localhost:8000';
 }
