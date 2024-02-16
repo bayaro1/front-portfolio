e05b0fcb16034306e76025863d3ee600f604b853
Ibai Arotçarena <arotcarena.ib@gmail.com>
2024-02-16 14:02:56 +0100

    design answer

--- app/mes-realisations/[projectId]/ui/Comments/CommentList.jsx
+++ app/mes-realisations/[projectId]/ui/Comments/CommentList.jsx
@@ -59,7 +59,7 @@ const CommentItem = ({comment}) => {
 
 const AnswerItem = ({answer}) => {
     return (
-        <div className="answer-item comment-item">
+        <div className={'answer-item comment-item' (answer.byAdmin ? ' by-admin': '')}>
             <div className="comment-item-header">
                 {
                     answer.byAdmin ? (
